0b6685ae1fc761f60ae36bfaf96d4138209f6d3d
Vivek Jain <vivekjain10@gmail.com>
2013-11-29 15:54:13 -0500

    Basic Peripheral setup

---
+++ Peripheral/Peripheral/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  Peripheral
+//
+//  Created by Vivek Jain on 11/29/13.
+//  Copyright (c) 2013 ThoughtWorks. All rights reserved.
+//
+
+#ifndef Peripheral_Constants_h
+#define Peripheral_Constants_h
+
+#define SERVICE_UUID           @"4736F4D8-F7E7-4A68-8FE9-CFF4D33F81E7"
+#define CHARACTERISTIC_UUID    @"E1B688E2-B558-4F8C-B3BA-85DF3577F669"
+
+#endif
--- Peripheral/Peripheral/TWViewController.m
+++ Peripheral/Peripheral/TWViewController.m
@@ -7,23 +7,94 @@
 //
 
 #import "TWViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "Constants.h"
 
-@interface TWViewController ()
-
+@interface TWViewController () <CBPeripheralManagerDelegate>
+@property (strong, nonatomic) CBPeripheralManager *peripheralManager;
+@property (strong, nonatomic) CBMutableCharacteristic *charateristic;
 @end
 
 @implementation TWViewController
 
+#pragma mark - View Lifecycle
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil options:nil];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.peripheralManager startAdvertising:@{CBAdvertisementDataServiceUUIDsKey: @[
+                                                       [CBUUID UUIDWithString:SERVICE_UUID]
+                                                       ]
+                                               }
+     ];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [self.peripheralManager stopAdvertising];
+    [super viewWillDisappear:animated];
+}
+
+#pragma mark - Peripheral Methods
+- (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral
+{
+    if(peripheral.state != CBPeripheralManagerStatePoweredOn) {
+        NSLog(@"Peripheral Manager is not Powered ON");
+        return;
+    }
+    
+    NSLog(@"Peripheral Manager is Powered ON");
+    
+    self.charateristic = [[CBMutableCharacteristic alloc] initWithType:[CBUUID UUIDWithString:CHARACTERISTIC_UUID]
+                                                            properties:CBCharacteristicPropertyNotify
+                                                                 value:nil
+                                                           permissions:CBAttributePermissionsReadable];
+    
+    CBMutableService *service = [[CBMutableService alloc] initWithType:[CBUUID UUIDWithString:SERVICE_UUID]
+                                                               primary:YES];
+    
+    service.characteristics = @[self.charateristic];
+    
+    [self.peripheralManager addService:service];
+}
+
+- (void)peripheralManager:(CBPeripheralManager *)peripheral
+                  central:(CBCentral *)central didSubscribeToCharacteristic:(CBCharacteristic *)characteristic
+{
+    NSLog(@"Central subscribed to characteristic");
+}
+
+- (void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didUnsubscribeFromCharacteristic:(CBCharacteristic *)characteristic
+{
+    NSLog(@"Cenral unsubscribed from characteristic");
+}
+
+- (void)peripheralManagerIsReadyToUpdateSubscribers:(CBPeripheralManager *)peripheral
+{
+    NSLog(@"Sending Again");
+    [self sendData];
+}
+
+- (void)peripheralManagerDidStartAdvertising:(CBPeripheralManager *)peripheral error:(NSError *)error
+{
+    if(error) {
+        NSLog(@"Failed to Advertise (%@)", error);
+    } else {
+        NSLog(@"Started Advertising");
+    }
+    
+}
+
+#pragma mark - Private
+- (void)sendData
+{
+    [self.peripheralManager updateValue:[@"someData" dataUsingEncoding:NSUTF8StringEncoding]
+                      forCharacteristic:self.charateristic
+                   onSubscribedCentrals:nil];
 }
 
 @end
